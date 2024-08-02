53deef13f20fd60c2f955afabd1e3b91b5d49192
wlsql852 <wlsql852@naver.com>
2024-08-02 17:54:07 +0900

    fix: Score 생성자(scoreid, studentid, subjectid) 추가

--- src/camp/model/Score.java
+++ src/camp/model/Score.java
@@ -11,8 +11,14 @@ public class Score {
     private ArrayList<Character> grade;
 
 
-    public Score(String seq) {
+    public Score(String seq, String studentId, String subjectId)
+    {
         this.scoreId = seq;
+        this.studentId = studentId;
+        this.subjectId = subjectId;
+        this.round = new ArrayList<>();
+        this.score = new ArrayList<>();
+        this.grade = new ArrayList<>();
     }
     public Score(String seq, String studentId, String subjectId, ArrayList<Integer> round,ArrayList<Integer> score,ArrayList<Character> grade){
         this.scoreId = seq;
@@ -35,33 +41,5 @@ public class Score {
     public ArrayList<Integer> getScore() {return score;}
 
     public ArrayList<Character> getGrade() {return grade;}
-
-    public static char changeGrade(String type, double avgScore) {
-        if(type.equals("MANDATORY")) {
-            if(avgScore>=95) return 'A';
-            else if(avgScore>=90) return 'B';
-            else if(avgScore>=80) return 'C';
-            else if(avgScore>=70) return 'D';
-            else if(avgScore>=60) return 'F';
-            else return 'N';
-        }
-        else if(type.equals("CHOICE")){
-            if(avgScore>=90) return 'A';
-            else if(avgScore>=80) return 'B';
-            else if(avgScore>=70) return 'C';
-            else if(avgScore>=60) return 'D';
-            else if(avgScore>=50) return 'F';
-            else return 'N';
-        }
-        else return 'N';
-    }
-
-    public void addScore(int round, int score, int subtype) {
-        this.round.add(round);
-        this.score.add(score);
-        if(subtype == 1) this.grade.add(changeGrade("MANDATORY",score));
-        else if(subtype == 2) this.grade.add(changeGrade("CHOICE",score));
-        else this.grade.add('N');  //이미 이전에 1,2가 아니면 들어오지 못하므로 안쓰는 코드
-    }
 }
 
