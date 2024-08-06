da9f27daf119be06eb6d1143a682b845d3e78017
Xabier <94465466+XabierJEONG@users.noreply.github.com>
2024-08-06 19:37:46 +0900

    클래스 분리

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -1,6 +1,7 @@
 package camp;
 
 import camp.model.*;
+import camp.CRUDOperations.Edit;
 
 import java.util.*;
 
@@ -411,97 +412,6 @@ public class CampManagementApplication {
         System.out.println("\n수강생 목록 조회 성공!");
     }
 
-
-    // 수강생 정보 수정
-    private static void updateStudent() {
-        System.out.println("\n수강생 정보를 수정합니다...");
-        // 수강생의 고유번호를 받아 이름, 상태를 수정
-        while (true) {
-            System.out.print("수강생의 고유번호 입력 : ");
-            String insertNumber = sc.next();
-            boolean bFindName = false;
-            // studentStore 에 입력한 고유번호이랑 같은 고유번호가 있다면 고유번호, 이름, 상태를 출력
-            for (int i = 0; i < studentStore.size(); i++) {
-                if (insertNumber.equals(studentStore.get(i).getStudentId())) {
-                    System.out.println("고유번호: " + studentStore.get(i).getStudentId() + ", 이름: " + studentStore.get(i).getStudentName()
-                            + ", 상태: " + studentStore.get(i).getStatus());
-                    System.out.println();
-                    bFindName = true;
-                    break;
-                }
-            }
-            if (!bFindName) { //존재하지 않는 고유번호라면
-                System.out.println("입력한 고유번호의 수강생은 없습니다.\n되돌아갑니다!");
-                break;
-            }
-            System.out.println("무엇을 변경하시겠습니까?");
-            System.out.print("번호를 입력해주세요 1.이름 2.상태 : ");
-            int updateNumber = 0;
-            try {
-                updateNumber = sc.nextInt();
-            } catch (Exception e) {
-                System.out.println("숫자로 입력해주세요");
-                sc.nextLine();
-            }
-            // 1. 이름 변경 선택
-            if (updateNumber == 1) {
-                System.out.print("무엇으로 이름을 변경하시겠습니까? : ");
-                String updateName = sc.next();
-
-                //입력한 고유번호(insertNumber)의 수강생의 이름을 setStudentName 메서드 이용해 updateName 으로 바꿔줘!
-                for (int i = 0; i < studentStore.size(); i++) {
-                    if (insertNumber.equals(studentStore.get(i).getStudentId())) {
-                        //입력한 이름이 기존 이름과 같을 때
-                        if (updateName.equals(studentStore.get(i).getStudentName())) {
-                            System.out.println("기존의 이름과 동일합니다. \n되돌아갑니다!");
-                            break;
-                        }
-                        studentStore.get(i).setStudentName(updateName);
-
-                        //변경된 수강생 내역 출력
-                        System.out.println("고유번호: " + studentStore.get(i).getStudentId() + ", 이름: " + studentStore.get(i).getStudentName()
-                                + ", 상태: " + studentStore.get(i).getStatus());
-                        System.out.println("\n수강생 정보 수정 성공!");
-                    }
-                }
-                break;
-
-                // 2. 상태 변경 선택
-            } else if (updateNumber == 2) {
-                System.out.print("무엇으로 상태를 변경하시겠습니까? (Green/Yellow/Red) : ");
-                String updateStatus = sc.next();
-                //updateStatus 이 Green/Yellow/Red 이외의 것을 입력했을 때
-                if (!(updateStatus.equals("Green") || updateStatus.equals("Yellow") || updateStatus.equals("Red"))) {
-                    System.out.println("입력하신 상태값은 없습니다. \n되돌아갑니다!");
-                    break;
-                }
-                //입력한 고유번호(insertNumber)의 수강생의 상태를 setStatus 메서드 이용해 updateStatus 으로 바꿔줘!
-                for (int i = 0; i < studentStore.size(); i++) {
-                    if (insertNumber.equals(studentStore.get(i).getStudentId())) {
-                        //입력한 상태가 기존 상태와 같을 때
-                        if (updateStatus.equals(studentStore.get(i).getStatus().name())) {
-                            System.out.println("기존의 상태와 동일합니다. \n되돌아갑니다!");
-                            break;
-                        }
-                        studentStore.get(i).setStatus(Status.valueOf(updateStatus));
-
-                        //변경한 수강생 내역 출력
-                        System.out.println("고유번호: " + studentStore.get(i).getStudentId() + ", 이름: " + studentStore.get(i).getStudentName()
-                                + ", 상태: " + studentStore.get(i).getStatus());
-                        System.out.println("\n수강생 정보 수정 성공!");
-                    }
-                }
-                break;
-
-                // 1 이름, 2 상태 이외 선택 시
-            } else {
-                System.out.println("잘못된 입력입니다.\n되돌아갑니다!");
-                break;
-            }
-        }
-    }
-
-
     private static void displayScoreView() {
         boolean flag = true;
         while (flag) {
@@ -707,72 +617,6 @@ public class CampManagementApplication {
         System.out.println("\n점수 등록 성공!");
     }
 
-    private static void updateRoundScoreBySubject() {
-        String studentId = getStudentId(); // 관리할 수강생 고유 번호
-        Student SI = null;
-        for (int i = 0; i < studentStore.size(); i++) {
-            if (studentId.equals(studentStore.get(i).getStudentId())) { //학생 아이디값 같으면 진행
-                SI = studentStore.get(i); //나중에 scoreDetail에 넣을 si 값을 찾은 id값을 대입
-                break;
-            }
-        }
-        if (SI == null) {
-            System.out.println("해당 학생을 찾을 수 없습니다.");
-            return; //오류시 끝내기
-        }
-
-        String inputSubjectId = getSubjectId(); //해당 과목 id 입력
-        Subject SJ = null;
-        for (int i = 0; i < subjectStore.size(); i++) {
-            if (inputSubjectId.equals(subjectStore.get(i).getSubjectId())) {
-                //입력된 과목과 불러온 과목 이름 같은지 대조
-                SJ = subjectStore.get(i);
-                //맞으면 해당 과목의 값을 넣고 다음으로 넘어감
-                break;
-            }
-        }
-        if (SJ == null) {
-            System.out.println("해당 과목을 찾을 수 없습니다.");
-            return; //오류
-        }
-        int inputRound;
-        while (true) {
-            System.out.println("수정할 회차를 입력해 주세요 ");
-            inputRound = sc.nextInt();
-            if (inputRound > 0 && inputRound < 11) {
-                System.out.println("선택한 회차 : " + inputRound + "회차 입니다");
-                break;
-            } else {
-                System.out.println("잘못된 회차 입니다. (1 ~ 10)회차 까지 있습니다.");
-            }
-        }
-        System.out.println(inputRound);
-        System.out.println("새로운 점수를 입력해 주세요 "); // 점수입력 - 범위벗어나는 숫자 입력시 오류 문자 내는 기능 넣기
-        int updatedScore = sc.nextInt();
-        for (int i = 0; i < scoreStore.size(); i++) {
-            if (SI.getStudentId().equals(scoreStore.get(i).getStudentId()) && SJ.getSubjectId().equals(scoreStore.get(i).getSubjectId())) { //
-                List<ScoreDetail> Sl = scoreStore.get(i).getScoreList(); //scoreStore에서 해당 학생아이디 과목아이디 를 가진 ScoreDetail 리스트를 가져온다.
-                int r = 0;
-                for (int j = 0; j < Sl.size(); j++) { // 해당 라운드가 값이 있는지 확인 하는 for 문
-                    if (Sl.get(j).getRound() == inputRound) {
-                        r = inputRound;
-                    }
-                }
-                if (r == 0) { // 해당 라운드 값이 없으면 r = 0 그대로 내려오므로 바로 값 추가하는것.
-                    ScoreDetail updatedRound = new ScoreDetail(inputRound, updatedScore, SJ.getSubjectType()); //새로운 다타일 생성
-                    scoreStore.get(i).addScore(updatedRound);
-                } else { //r ==inputRound 가 됐으면 해당 스코어값을 수정하는 코드
-                    for (int k = 0; k < Sl.size(); k++) { //Sl의 k위치의 라운드 값과 r의 입력된 라운드 값이 매치되는 위치를 찾고 그위치의 값을 setScore해준다.
-                        if (r == Sl.get(k).getRound()) {
-                            Sl.get(k).setScore(SJ.getSubjectType(), updatedScore);
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println("시험 점수를 수정합니다...");
-        System.out.println("\n점수 수정 성공!");
-    }
 
     // 수강생의 특정 과목 회차별 등급 조회
     private static void inquireRoundGradeBySubject() {
@@ -988,35 +832,6 @@ public class CampManagementApplication {
             }
         }
         if (!found) System.out.println("해당 상태의 학생이 존재하지 않습니다.");
-    }
-
-
-    //수강생 지우기
-    public static void removeStudent() {
-        System.out.println("삭제할 수강생의 고유 번호를 입력하세요");
-        String studentId = sc.next();
-
-        //지운게 성공한적 체크를 위해 boolean 형 지역변수 선언
-        boolean successRemove = false;
-        for (Student student : studentStore) {
-            if (student.getStudentId().equals(studentId)) {
-                studentStore.remove(student);
-                //지우기 성공, 어차피 학생 ID 는 유일하니 바로 Break;
-                successRemove = true;
-                break;
-            }
-        }
-
-        //점수 저장 공간에서도 삭제
-        //그냥 Index 0 으로 하고 지워주면 삭제하면서 ArrayList 배열 땡겨져서 안됨, 뒤에서부터 지워야 배열 안변함
-        for (int i = scoreStore.size() - 1; i >= 0; i--) {
-            if (scoreStore.get(i).getStudentId().equals(studentId)) {
-                scoreStore.remove(i);
-            }
-        }
-
-        if (successRemove) System.out.println("성공적으로 삭제 되었습니다.");
-        else System.out.println("그런 수강생 없습니다");
     }
 
     //특정 과목 특정 회차 점수 순위
